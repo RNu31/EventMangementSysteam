ed6c3558e6e537445c89e37624e0ac892a126e46
RNu31 <143842642+RNu31@users.noreply.github.com>
2024-09-12 09:51:20 -0600

    feat:integrar menu prueva a divelop

--- Src/mainMaxicom.cc
+++ Src/mainMaxicom.cc
@@ -1,7 +1,18 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <time.h>
+
+void Main_Menu();
 
 int main()
 {
 
 return 0;
+}
+
+void Main_Menu(){
+std::cout<<"---------------------------------------------"<<std::endl;
+std::cout<<"|                    M E N U                 "<<std::endl;
+std::cout<<"---------------------------------------------"<<std::endl;
 }
